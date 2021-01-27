78c6ceb7855625ead4d12960531edd4ace8ff6e4
dangvannhang <dangvannhang107@gmail.com>
2021-01-27 19:57:11 +0700

    Fix about 500 server

---
+++ README.md
@@ -0,0 +1,9 @@
+## run app
+- Update composer
++ composer update
+- Install npm
++ npm install
+- Run migrate
++ php artisan migrate
+- Run seeder
++ php artisan db:seed
--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,8 @@ const mix = require('laravel-mix');
 
 
     mix.js('resources/js/app.js', 'public/js')
+    .sourceMaps()
     .js('resources/js/bootstrap.js', 'public/js')
+    // .js('resources/js/app.js', 'public/js').sourceMaps()
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
